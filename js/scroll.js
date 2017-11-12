4a1cfcde4c8d2303562637011e5b1386ed7a26a0
kir9819 <kirill9819@mail.ru>
2017-11-12 22:32:37 +0300

    12.11.17 22:32

--- js/scroll.js
+++ js/scroll.js
@@ -1,42 +1,70 @@
 $(document).ready(function(){
   var oldScrollY = 0;
+  var scrolled = 0;
   var id = "";
   var timeScroll = 2000;
   var check = true;
+  console.log("window" + window.innerHeight);
+  //function stop() {check = true;} 
   
   window.onscroll = function(event){
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (check) {var dY = scrolled - oldScrollY;
-  check = false;}
-
-  console.log(dY);
+	  oldScrollY = scrolled;
+	  scrolled = $(window).scrollTop();
+	  console.log("old " + oldScrollY);
+	  console.log("new " + scrolled);
+	  if ($('html, body').is(':animated')) { // <-рср
+      return;
+    };
+  //scrolled = $(window).scrollTop(); //= window.pageYOffset || document.documentElement.scrollTop;
+  //console.log("new " + scrolled);
+  //if (check) {var dY = scrolled - oldScrollY;
+  //check = false;}
+  var dY = scrolled - oldScrollY;
+  //console.log("dY " + dY);
+  //console.log("w " + $(window).scrollTop());
+  //if($(window).scrollTop() == window.innerHeight) { id = "centerBlock"}
+  //if($(window).scrollTop() == 0) { id = ""}
+  //if($(window).scrollTop() > 2*window.innerHeight) {id="Block2"}
   //console.log($(window));
   //if($(window).scrollTop() < window.innerHeight) {
   switch(id) {
   case "": 
-		if(dY > 0) {
+		if(dY > 1) {
 		$('html, body').animate({scrollTop:$("#centerBlock").offset().top}, timeScroll);
+		//if($(window).scrollTop() == window.innerHeight-1){id = "centerBlock";}
 		id = "centerBlock";
-		check = true;}
-		break;
-  case "centerBlock":
-		if(dY > 0) {
-		$('html, body').animate({scrollTop:$("#Block2").offset().top}, timeScroll);
-		id = "Block2";
+		
+		//check = true;}
 		
 		}
-		else { $('html, body').animate({scrollTop:0}, timeScroll);
+		console.log("___________0");
+		break;
+  case "centerBlock":
+		if(dY > 1) {
+		$('html, body').animate({scrollTop:$("#Block2").offset().top}, timeScroll);
+		//if ($(window).scrollTop() == 2*window.innerHeight-1){id = "Block2";}
+		console.log("try down");
+		id = "Block2";
+		}
+		else if (dY < 0) { $('html, body').animate({scrollTop:0}, timeScroll);
+		//if ($(window).scrollTop() == 0) {id = "";}
+		console.log("try up");
 		id = "";
-		var dY = scrolled - oldScrollY;}
+		//var dY = scrolled - oldScrollY;}
+        }
+		console.log("___________1");
 		break;
   case "Block2":
 		if(dY < 0) {
 		$('html, body').animate({scrollTop:$("#centerBlock").offset().top}, timeScroll);
-		id="centerBlock";
+		//if($(window).scrollTop() == window.innerHeight) {id="centerBlock";}
+		id = "centerBlock";
+		//oldScrollY = scrolled;
 		}
+		console.log("___________2");
 		break;
   }
-  oldScrollY = scrolled
+  //oldScrollY = scrolled;
   //if (dY > 0) { 
   // var position = $("#centerBlock").offset().top; 
   //$('html, body').animate({scrollTop:position}, 1000); 
---
+++ js/scroll.old.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  var oldScrollY = 0;
+  var scrolled = 0;
+  var id = "";			// id активного элемента для свича
+  var timeScroll = 2000; // время скролла
+  
+  window.onscroll = function(event){
+	  oldScrollY = scrolled;
+	  scrolled = $(window).scrollTop();
+	  console.log("old " + oldScrollY);
+	  console.log("new " + scrolled);
+	  if ($('html, body').is(':animated')) { // <- Если анимация работает, 
+      return;								// 		выход из функции
+    };
+  var dY = scrolled - oldScrollY;
+  switch(id) {
+  case "": 
+		if(dY > 1) {
+		$('html, body').animate({scrollTop:$("#centerBlock").offset().top}, timeScroll);
+		id = "centerBlock";
+		}
+		console.log("___________0");
+		break;
+  case "centerBlock":
+		if(dY > 1) {
+		$('html, body').animate({scrollTop:$("#Block2").offset().top}, timeScroll);
+		console.log("try down");
+		id = "Block2";
+		}
+		else if (dY < 0) { $('html, body').animate({scrollTop:0}, timeScroll);
+		console.log("try up");
+		id = "";
+        }
+		console.log("___________1");
+		break;
+  case "Block2":
+		if(dY < 0) {
+		$('html, body').animate({scrollTop:$("#centerBlock").offset().top}, timeScroll);
+		id = "centerBlock";
+		}
+		console.log("___________2");
+		break;
+  }
+  };
+});
